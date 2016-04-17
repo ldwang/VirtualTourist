cd2e97aff1ff0704722a49ba882b4a6a81fcd6ce
Long(Daniel) Wang <wanglong@gmail.com>
2016-04-17 16:49:47 -0600

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Long Wang on 2016-04-17.
+//  Copyright © 2016 Long Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
