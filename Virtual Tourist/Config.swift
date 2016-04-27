aa6a124412c5500332258e536684ac779fb430f6
Long(Daniel) Wang <wanglong@gmail.com>
2016-04-27 15:39:47 -0600

    Add framework of the VTDB

---
+++ Virtual Tourist/Config.swift
@@ -0,0 +1,9 @@
+//
+//  Config.swift
+//  Virtual Tourist
+//
+//  Created by Long Wang on 2016-04-24.
+//  Copyright © 2016 Long Wang. All rights reserved.
+//
+
+import Foundation
--- Virtual Tourist/TravelLocationsMapViewController.swift
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationsMapViewController.swift
 //  Virtual Tourist
 //
 //  Created by Long Wang on 2016-04-17.
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
